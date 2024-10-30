02adb5059a7810f03ce6c23ede7166e024420b34
enjelia220 <enjeliafarelayufadila@gmail.com>
2024-10-30 16:54:36 +0700

    comen

---
+++ src/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"GajiKaryawan","l":"GajiKaryawan()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"GajiKaryawan","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
